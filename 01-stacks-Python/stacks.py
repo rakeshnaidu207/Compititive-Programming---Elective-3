bf9a05bb23d5d293b7a2c07b0590a64cf8be2e61
rakeshnaidu207 <72121361+rakeshnaidu207@users.noreply.github.com>
2021-07-15 14:44:51 +0530

    commited

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -48,7 +48,7 @@ class stack(object):
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
-        self.ll.delete_first
+        self.ll.delete_first()
         
        
     
