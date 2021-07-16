e0f5121318e8341fd1fe9206aafd60d19bc96e66
rakeshnaidu207 <72121361+rakeshnaidu207@users.noreply.github.com>
2021-07-16 12:28:10 +0530

    commited

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -2,5 +2,11 @@
 Input a list.
 Output a sorted list."""
 def quicksort(array):
-	# Your code goes here
-	pass
+    if (len(array) < 2):
+        return array
+    else:
+        first = array[0]  # pivot
+        rest = array[1:]
+        lo = [x for x in rest if x < first]
+        hi = [x for x in rest if x >= first]
+        return quicksort(lo) + [first] + quicksort(hi)
