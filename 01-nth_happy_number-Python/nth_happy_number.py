a1f24566a64f9969142fedc049afa7450a7f1bd8
rakeshnaidu207 <72121361+rakeshnaidu207@users.noreply.github.com>
2021-07-22 19:12:31 +0530

    Update nth_happy_number.py

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -16,4 +16,44 @@
 
 
 def nth_happy_number(n):
+  def numSquareSum(n):
+    squareSum = 0;
+    while(n):
+        squareSum += (n % 10) * (n % 10);
+        n = int(n / 10);
+    return squareSum;
+ 
+# method return true if
+# n is Happy number
+def ishappynumber(n):
+ 
+    # initialize slow
+    # and fast by n
+    slow = n;
+    fast = n;
+    while(True):
+         
+        # move slow number
+        # by one iteration
+        slow = numSquareSum(slow);
+ 
+        # move fast number
+        # by two iteration
+        fast = numSquareSum(numSquareSum(fast));
+        if(slow != fast):
+            continue;
+        else:
+            break;
+ 
+    # if both number meet at 1,
+    # then return true
+    return (slow == 1);
+def nthhappynumber(n):
+	found = 0
+	guess = 0
+	while (found <= abs(n)):
+		guess += 1
+		if(nthhappyprime(guess)):
+			found += 1
+	return guess
 
