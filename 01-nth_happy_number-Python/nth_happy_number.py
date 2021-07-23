6452a1da0cb8a9d0c18c9b1f3172be51413593c7
rakeshnaidu207 <72121361+rakeshnaidu207@users.noreply.github.com>
2021-07-23 16:33:16 +0530

    commited

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -17,77 +17,23 @@ import nth_happy_number
 # assert(nth_happy_number(8) == 31)
 
 
-
-#helper function to find the squares of digits
-def numSquareSum(n): 
-    squareSum = 0; 
-    while(n): 
-        squareSum += (n % 10) * (n % 10); 
-        n = int(n / 10); 
-    return squareSum; 
-  
-#to find out if it is a happy number or not
-def isHappynumber(n): 
-    slow = n; 
-    fast = n; 
-    while(True): 
-        slow = numSquareSum(slow); 
-        fast = numSquareSum(numSquareSum(fast)); 
-        if(slow != fast): 
-            continue; 
-        else: 
-            break; 
-    return (slow == 1); 
-#finding nth happy primes
-
+def Happynum(n):
+    sum = 0
+    while(n!=0):
+        sum += (n%10)**2
+        n//=10
+    if sum == 1:
+        return True
+    elif sum<10:
+        return False
+    else:
+        return Happynum(sum)
 def nth_happy_number(n):
-    found = 0
-    guess = 0
-    while (found <= n):
-        guess += 1
-        #print(guess)
-        if (isHappynumber(guess)):
-            found += 1
-            #print(guess)
-    return guess
-def numSquareSum(n):
-    squareSum = 0;
-    while(n):
-        squareSum += (n % 10) * (n % 10);
-        n = int(n / 10);
-    return squareSum;
- 
-# method return true if
-# n is Happy number
-def ishappynumber(n):
- 
-    # initialize slow
-    # and fast by n
-    slow = n;
-    fast = n;
-    while(True):
-         
-        # move slow number
-        # by one iteration
-        slow = numSquareSum(slow);
- 
-        # move fast number
-        # by two iteration
-        fast = numSquareSum(numSquareSum(fast));
-        if(slow != fast):
-            continue;
-        else:
-            break;
- 
-    # if both number meet at 1,
-    # then return true
-    return (slow == 1);
-def nthhappynumber(n):
-	found = 0
-	guess = 0
-	while (found <= abs(n)):
-		guess += 1
-		if(nth_happy_number(guess)):
-			found += 1
-	return guess
+    f = 1
+    g = 0
+    while(f<=abs(n)):
+        g+=1
+        if(Happynum(g)):
+            f+=1
+    return g
 
