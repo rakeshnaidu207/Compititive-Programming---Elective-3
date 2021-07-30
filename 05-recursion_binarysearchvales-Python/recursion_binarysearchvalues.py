c5ffcc9f928aa7a19014fcb36c58a50735cf6846
rakeshnaidu207 <72121361+rakeshnaidu207@users.noreply.github.com>
2021-07-30 14:22:29 +0530

    commited

--- 05-recursion_binarysearchvales-Python/recursion_binarysearchvalues.py
+++ 05-recursion_binarysearchvales-Python/recursion_binarysearchvalues.py
@@ -18,6 +18,24 @@
 # Hint: Do not slice the list L, but rather adjust the indexes into L. 
 
 def recursion_binarysearchvalues(L, v):
-	# Your codes goes here
-	pass
-	
+    return binarysearch(L,v,0,len(L)-1)
+    
+def binarysearch(L,v,low,high,res=[]):
+    mid=(low+high)//2 #0,1
+    if (L[mid] == v and low<high):#acfgmq #a
+        res.append((mid,L[mid]))
+        return res
+    elif(low == high):#1,1
+        res.append((mid,L[mid]))
+        return res
+    elif(v<L[mid]):#a<f
+        high=mid-1 #1 low 0
+        res.append((mid,L[mid]))#mid=0
+    else:
+        low=mid+1
+        res.append((mid,L[mid]))
+    return binarysearch(L,v,low,high,res)
+
+#L = ['a', 'c', 'f', 'g', 'm', 'q']
+#v = 'z'
+#print(recursion_binarysearchvalues(L,v))
