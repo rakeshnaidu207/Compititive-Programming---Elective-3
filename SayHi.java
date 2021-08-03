eeb9e135b1f1960f5d90a5fe0e57e9316b773e9a
rakeshnaidu207 <72121361+rakeshnaidu207@users.noreply.github.com>
2021-08-03 16:58:10 +0530

    commited

--- SayHi.java
+++ SayHi.java
@@ -6,5 +6,21 @@
 
 
 public class SayHi {
-    
+    String name;
+    int age;
+    SayHi(String n, int i){
+        name=n;
+        age=i;
+    }
+    public void Display(){
+        System.out.println("Hi"+","+"my name is"+" "+name+" "+"and"+" "+"I am "+age+" "+"years old!" );
+    }
+
+    public static void main(String args[]) {
+        SayHi d1=new SayHi("Dot",4);
+        SayHi d2=new SayHi("ELF",3);
+        d1.Display();
+        d2.Display();
+        
+    }
 }
